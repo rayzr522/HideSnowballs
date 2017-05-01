ac667f6a8e18ef7ecc73582935cb1f1834f86236
Rayzr522 <rayzr522@gmail.com>
2017-05-01 08:56:45 -0400

    Done.

--- src/main/java/com/rayzr522/hidesnowballs/HideSnowballs.java
+++ src/main/java/com/rayzr522/hidesnowballs/HideSnowballs.java
@@ -1,110 +1,39 @@
 package com.rayzr522.hidesnowballs;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.logging.Level;
-
-import org.bukkit.configuration.file.YamlConfiguration;
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketContainer;
+import com.comphenix.protocol.events.PacketEvent;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import com.rayzr522.creativelynamedlib.config.Messages;
 
 /**
  * @author Rayzr
  */
 public class HideSnowballs extends JavaPlugin {
-    private static HideSnowballs instance;
-
-    private Messages lang = new Messages();
+    private ProtocolManager protocolManager;
 
     @Override
     public void onEnable() {
-        instance = this;
+        protocolManager = ProtocolLibrary.getProtocolManager();
 
-        reload();
-    }
-    
-    @Override
-    public void onDisable() {
-        instance = null;
-    }
-    
-    /**
-     * (Re)loads all configs from the disk
-     */
-    public void reload() {
-        saveDefaultConfig();
-        reloadConfig();
-        
-        lang.load(getConfig("messages.yml"));
-    }
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.HIGH, PacketType.Play.Server.SPAWN_ENTITY) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        PacketContainer packet = event.getPacket();
 
-    /**
-     * If the file is not found and there is a default file in the JAR, it saves the default file to the plugin data folder first
-     * 
-     * @param path The path to the config file (relative to the plugin data folder)
-     * @return The {@link YamlConfiguration}
-     */
-    public YamlConfiguration getConfig(String path) {
-        if (!getFile(path).exists() && getResource(path) != null) {
-            saveResource(path, true);
-        }
-        return YamlConfiguration.loadConfiguration(getFile(path));
-    }
-    
-    /**
-     * Attempts to save a {@link YamlConfiguration} to the disk, and any {@link IOException}s are printed to the console
-     * 
-     * @param config The config to save
-     * @param path The path to save the config file to (relative to the plugin data folder)
-     */
-    public void saveConfig(YamlConfiguration config, String path) {
-        try {
-            config.save(getFile(path));
-        } catch (IOException e) {
-            getLogger().log(Level.SEVERE, "Failed to save config", e);
-        }
-    }
-
-    /**
-     * @param path The path of the file (relative to the plugin data folder)
-     * @return The {@link File}
-     */
-    public File getFile(String path) {
-        return new File(getDataFolder(), path.replace('/', File.pathSeparatorChar));
-    }
-    
-    /**
-     * Returns a message from the language file
-     * 
-     * @param key The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String tr(String key, Object... objects) {
-        return lang.tr(key, objects);
-    }
-
-    /**
-     * Returns a message from the language file without adding the prefix
-     * 
-     * @param key The key of the message to translate
-     * @param objects The formatting objects to use
-     * @return The formatted message
-     */
-    public String trRaw(String key, Object... objects) {
-        return lang.trRaw(key, objects);
-    }
-
-    /**
-     * @return The {@link Messages} instance for this plugin
-     */
-    public Messages getLang() {
-        return lang;
-    }
-    
-    public static HideSnowballs getInstance() {
-        return instance;
+                        Entity entity = packet.getEntityModifier(event).read(0);
+                        if (entity != null && entity.getType() == EntityType.SNOWBALL) {
+                            event.setCancelled(true);
+                        }
+                    }
+                }
+        );
     }
 
 }
